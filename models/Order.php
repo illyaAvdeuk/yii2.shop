a468a41ed32f4bd7796b70d9997b4a6af7e8385c
Illya Avdeyuk <redfabricius@gmail.com>
2016-12-01 18:29:47 +0200

    3rd from SS

--- models/Order.php
+++ models/Order.php
@@ -29,7 +29,19 @@ class Order extends ActiveRecord
     {
         return 'order';
     }
-
+    public function behaviors() {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                // если вместо метки времени UNIX используется datetime:
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
     /**
      * @inheritdoc
      */
