e5debc45481b15eb56b38ec8b294345b23e6dade
Illya Avdeyuk <redfabricius@gmail.com>
2016-12-02 17:33:41 +0200

    4th commit from home - test

--- views/cart/view.php
+++ views/cart/view.php
@@ -30,11 +30,11 @@
                     </tr>
                 <?php endforeach?>
                 <tr>
-                    <td colspan="4">Итого: </td>
+                    <td colspan="5">Итого: </td>
                     <td><?= $session['cart.qty']?></td>
                 </tr>
                 <tr>
-                    <td colspan="4">На сумму: </td>
+                    <td colspan="5">На сумму: </td>
                     <td><?= $session['cart.sum']?></td>
                 </tr>
                 </tbody>
